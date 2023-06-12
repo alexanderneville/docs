22107fc7fd8b0a5d111f03f74bb7053e513631cc
Alexander Neville <dev@alexneville.co.uk>
2023-06-12 22:34:05 +0100

    add js scripts

--- build.sh
+++ build.sh
@@ -2,8 +2,12 @@
 
 mkdir -p out
 rm -rf out/*
-cp -r res out/
-find . \( -name '.git' -o -name 'out' -o -name 'res' -o -name 'dot' \) -prune -o -type d -exec mkdir -p out/{} \;
+mkdir -p out/res
+cp -r src/res/* out/res/
+cp -r res/* out/res/
+find src \( -name '.git' -o -name 'out' -o -name 'res' -o -name 'dot' \) -prune -o -type d -exec mkdir -p out/{} \;
 build="pandoc -f markdown -t html -s --mathjax=https://cdn.alexneville.co.uk/mathjax/tex-svg.js --data-dir="./" --template=pandoc-format --lua-filter=md-to-html-links.lua --filter=pandoc-crossref --metadata-file=./metadata.yaml --no-highlight -o"
-find . -mindepth 2 -type f -name '*.md' -exec $build out/{} {} \;
+find src -mindepth 2 -type f -name '*.md' -exec $build out/{} {} \;
 for f in $(find out -iname '*.md' -type f -print); do mv "$f" ${f%.md}.html; done
+mv out/src/* out/
+rm -rf out/src
---
+++ res/script.js
@@ -0,0 +1,58 @@
+async function copyCode(block, copyButton) {
+  let text = block.innerText;
+  await navigator.clipboard.writeText(text);
+  let para = copyButton.querySelector("p")
+  para.innerHTML = null;
+  let icon = document.createElement("i")
+  icon.classList = "nf nf-md-clipboard_check_outline";
+  let message = document.createTextNode(" Copied!");
+  para.appendChild(icon);
+  para.appendChild(message);
+  setTimeout(
+    (copyButton) => {
+      let para = copyButton.querySelector("p")
+      let icon = document.createElement("i")
+      icon.classList = "nf nf-md-clipboard_text_outline";
+      para.innerHTML = null;
+      para.appendChild(icon);
+    },
+    500,
+    copyButton
+  );
+}
+
+function addClipboardItems() {
+  let blocks = document.getElementsByTagName("pre");
+  for (var i = 0; i < blocks.length; i++) {
+    if (navigator.clipboard) {
+      let copyButton = document.createElement("div");
+      copyButton.classList = "copyButton";
+      let para = document.createElement("p")
+      let icon = document.createElement("i");
+      icon.classList = "nf nf-md-clipboard_text_outline";
+      para.appendChild(icon);
+      copyButton.appendChild(para);
+      blocks[i].appendChild(copyButton);
+      let block = blocks[i].querySelector("code")
+      copyButton.addEventListener("click", async () => {
+        await copyCode(block, copyButton);
+      });
+    }
+  }
+}
+
+function addAnchorLinks() {
+  let headings = document.querySelectorAll("h1, h2, h3, h4, h5, h6");
+  for (let i = 0; i < headings.length; i++) {
+    let link = document.createElement("a");
+    link.href = `#${headings[i].id}`;
+    let icon = document.createElement("i");
+    icon.classList = "nf nf-oct-link";
+    link.appendChild(icon);
+    headings[i].appendChild(document.createTextNode(" "));
+    headings[i].appendChild(link);
+  }
+}
+
+addAnchorLinks();
+addClipboardItems();
